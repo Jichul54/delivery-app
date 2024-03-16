5b548a1034b932258f284508bdde32fd9e612ed1
yfujinaka <yuya20021115@gmail.com>
2024-03-16 21:19:39 +0900

    ログイン画面　編集

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,28 @@
-import React, { useEffect } from 'react';
+// import logo from './logo.svg';
+import './App.css';
+import LoginForm from './LoginForm';
 
 function App() {
-  useEffect(() => {
-    fetch('/api/test/')
-      .then(response => response.json())
-      .then(data => console.log(data))
-      .catch(error => console.error('Error fetching data: ', error));
-  }, []);
+  return (
+    <div className="App">
+      {/* <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          React
+        </a>
+      </header> */}
 
-  return <div>Check the console for the API response.</div>;
+      <LoginForm></LoginForm>
+    </div>
+  );
 }
 
 export default App;
