3f15c86298cf4afb80eea5069a5ae3f42513a429
Akaumigame <106215296+akaumigame6@users.noreply.github.com>
2024-03-17 15:30:53 +0900

    配達ステータステーブルの追加

---
+++ backend/delivery_app/models/delivery_status.py
@@ -0,0 +1,7 @@
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+class Dalivery_Status(models.Model):
+    derivery_status_name = models.CharField(_('derivery_status_name'), max_length=150, unique=True)
+    def __str__(self):
+        return self.derivery_status
