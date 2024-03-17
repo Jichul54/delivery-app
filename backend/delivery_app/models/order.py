65723422aa07012b58e5686e58960161e2cb9946
Akaumigame <106215296+akaumigame6@users.noreply.github.com>
2024-03-17 21:23:41 +0900

    配送のテーブル作成 全ての仮データの入力

--- backend/delivery_app/models/order.py
+++ backend/delivery_app/models/order.py
@@ -5,7 +5,7 @@ from .merchandise import Merchandise
 
 class Order(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    Merchandise = models.ForeignKey(Merchandise, on_delete=models.CASCADE)
+    merchandise = models.ForeignKey(Merchandise, on_delete=models.CASCADE)
     merchandise_count = models.IntegerField(_('merchandise_count'),null=False)
     delivery_date = models.DateField(_('delivery_date'),null=False)
     def __str__(self):
