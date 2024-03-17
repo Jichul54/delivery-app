1ccc4d12a727194164b80f6b38a711302f3ad329
Akaumigame <106215296+akaumigame6@users.noreply.github.com>
2024-03-17 15:46:40 +0900

    derivery_statusにミスがあったためマイグレーションを戻してやり直した

--- backend/delivery_app/models/delivery_status.py
+++ backend/delivery_app/models/delivery_status.py
@@ -4,4 +4,4 @@ from django.utils.translation import gettext_lazy as _
 class Dalivery_Status(models.Model):
     derivery_status_name = models.CharField(_('derivery_status_name'), max_length=150, unique=True)
     def __str__(self):
-        return self.derivery_status
+        return self.derivery_status_name
