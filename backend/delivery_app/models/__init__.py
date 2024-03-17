87f63789c6e337e4b390e9486636815b483d2831
Akaumigame <106215296+akaumigame6@users.noreply.github.com>
2024-03-17 17:59:42 +0900

    商品のテーブル、仮データ入力

--- backend/delivery_app/models/__init__.py
+++ backend/delivery_app/models/__init__.py
@@ -3,3 +3,4 @@ from .company import Company
 from .delivery_status import Delivery_Status
 from .sales_office import Sales_office
 from .driver import Driver
+from .merchandise import Merchandise
---
+++ backend/delivery_app/models/merchandise.py
@@ -0,0 +1,10 @@
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+from .company import Company
+
+class Merchandise(models.Model):
+    company = models.ForeignKey(Company, on_delete=models.CASCADE)
+    merchandise_name = models.CharField(_('merchandise_name'), max_length=250, unique=True)
+    warehouse_address = models.CharField(_('Warehouse_address'), max_length=250, unique=True)
+    def __str__(self):
+        return self.merchandise_name
