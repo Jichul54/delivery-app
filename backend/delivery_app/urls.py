f97fc98dd6a54f91d95acfe6501721b8506629c9
Yoshimune101 <yoshimune101asano@gmail.com>
2024-03-18 00:14:46 +0900

    drivers, loginのAPI作成

--- backend/delivery_app/urls.py
+++ backend/delivery_app/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import TestAPIView
+from .views import TestAPIView, LoginVendorsView, DriversView
 
 urlpatterns = [
     path('api/test/', TestAPIView.as_view(), name='test-api'),
+    path('login-vendors', LoginVendorsView.as_view(), name='login-vendors'),
+    path('drivers', DriversView.as_view(), name='drivers'),
 ]
--- backend/delivery_app/views.py
+++ backend/delivery_app/views.py
@@ -1,8 +1,41 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.authtoken.models import Token
+from django.contrib.auth import authenticate
 
 class TestAPIView(APIView):
     def get(self, request, format=None):
         data = {"message": "Hello from Django!"}
         return Response(data, status=status.HTTP_200_OK)
+
+class LoginVendorsView(APIView):
+    def post(self, request):
+        username = request.data.get('username')
+        password = request.data.get('password')
+
+        # ユーザーの認証を試みる
+        user = authenticate(username=username, password=password)
+
+        if user is not None:
+            # ユーザーが認証された場合、トークンを生成する
+            token, created = Token.objects.get_or_create(user=user)
+
+            # トークンを返す
+            return Response({"token": token.key}, status=status.HTTP_200_OK)
+        else:
+            # ユーザーが認証されなかった場合はエラーを返す
+            return Response({"message": "Invalid username or password"}, status=status.HTTP_401_UNAUTHORIZED)
+
+class DriversView(APIView):
+    def get(self, request):
+        sales_office_id = request.GET.get('sales_office_id')
+        if sales_office_id:
+            return Response({"sales_office_id": sales_office_id},  status=status.HTTP_200_OK)
+        else:
+            return Response({"error": "sales_office_id not provided"}, status=status.HTTP_400_BAD_REQUEST)
+
+
+
+
+
