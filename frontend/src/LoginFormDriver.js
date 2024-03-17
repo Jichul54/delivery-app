ffad12ba801b89b4c5fb37f7045279a8cf5cf0ea
yfujinaka <yuya20021115@gmail.com>
2024-03-17 14:14:31 +0900

    edited indent

--- frontend/src/LoginFormDriver.js
+++ frontend/src/LoginFormDriver.js
@@ -12,40 +12,40 @@ export const LoginForm = () => {
   return (
     <div className='Login'>
       <Container maxWidth='xs'>
-          <Card variant='outlined' sx={{ p:2, my:25 }}>
-            <CardHeader title='ログイン'/>
-            <CardContent
-              sx={{
-                display: 'grid',
-                gap: 2,
-                gridTemplateRows: 'repeat(3, 1fr)'
-              }}
+        <Card variant='outlined' sx={{ p:2, my:25 }}>
+          <CardHeader title='ログイン'/>
+          <CardContent
+            sx={{
+              display: 'grid',
+              gap: 2,
+              gridTemplateRows: 'repeat(3, 1fr)'
+            }}
+          >
+            <TextField
+              id='username'
+              label='ID'
+              variant='outlined'
+              size='medium'
+              required
+            />
+            <TextField
+              id='password'
+              label='パスワード'
+              variant='outlined'
+              size='medium'
+              required
+            />
+            <Button
+              size='small'
+              color='primary'
+              variant='contained'
+              sx={{ mx:10 }}
             >
-              <TextField
-                id='username'
-                label='ID'
-                variant='outlined'
-                size='medium'
-                required
-              />
-              <TextField
-                id='password'
-                label='パスワード'
-                variant='outlined'
-                size='medium'
-                required
-              />
-              <Button
-                size='small'
-                color='primary'
-                variant='contained'
-                sx={{ mx:10 }}
-              >
-                ログイン
-              </Button>
-            </CardContent>
-          </Card>
-        </Container>
+              ログイン
+            </Button>
+          </CardContent>
+        </Card>
+      </Container>
     </div>
   );
 }
