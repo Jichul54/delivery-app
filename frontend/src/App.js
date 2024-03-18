d1d16fc03787ffe74dcfa464aeab7e5b68162a38
Jichul54 <kjc778869@gmail.com>
2024-03-18 17:49:00 +0900

    add frontend test code

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,11 +2,13 @@ import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import SignUpForm from './pages/Signup/SignUpForm';
 import LoginFormUser from './pages/Login/LoginFormUser';
+import Test from './pages/Test/Test';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path={`/test`} element={<Test />} />
         <Route path={`/`} element={<LoginFormUser />} />
         <Route path={`/CreateAccount`} element={<SignUpForm />} />
       </Routes>
