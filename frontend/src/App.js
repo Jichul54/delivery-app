0305e022d0ec6d5341dd7f2518ee27cb93b35661
yfujinaka <yuya20021115@gmail.com>
2024-03-18 23:06:04 +0900

    コンフリクト解消

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,3 @@
-import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import SignUpForm from './pages/Signup/SignUpForm';
 import LoginFormUser from './pages/Login/LoginFormUser';
@@ -6,23 +5,13 @@ import Test from './pages/Test/Test';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React!!!
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path={`/test`} element={<Test />} />
+        <Route path={`/`} element={<LoginFormUser />} />
+        <Route path={`/createAccount`} element={<SignUpForm />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
-
 export default App;
