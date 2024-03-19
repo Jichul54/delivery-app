5b3a354dd2ea8aeb22c9d8db02667a8a025ed4d1
Yoshimune101 <yoshimune101asano@gmail.com>
2024-03-19 22:01:13 +0900

    処理修正

--- backend/delivery_app/urls.py
+++ backend/delivery_app/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 from .views import TestAPIView, LoginVendorsView, DriversView, UsersView
+from django.urls import path
+from .views import CustomTokenObtainPairView
 
 urlpatterns = [
     path('api/test/', TestAPIView.as_view(), name='test-api'),
     path('login-vendors', LoginVendorsView.as_view(), name='login-vendors'),
     path('drivers', DriversView.as_view(), name='drivers'),
     path('users', UsersView.as_view(), name='users'),
+    path('token', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
 ]
--- backend/delivery_app/views.py
+++ backend/delivery_app/views.py
@@ -3,6 +3,11 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.authtoken.models import Token
 from django.contrib.auth import authenticate
+from rest_framework.permissions import IsAuthenticated
+from .models.user import User
+from .serializers import UserSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework.permissions import AllowAny
 
 class TestAPIView(APIView):
     def get(self, request, format=None):
@@ -38,15 +43,19 @@ class DriversView(APIView):
             return Response({"error": "sales_office_id not provided"}, status=status.HTTP_400_BAD_REQUEST)
 
 class UsersView(APIView):
-    def get(self, request):
-        address = request.GET.get('address')
-        if address:
-            return Response({"address": address},  status=status.HTTP_200_OK)
-        else:
-            return Response({"error": "address not provided"}, status=status.HTTP_400_BAD_REQUEST)
+    # permission_classes = [IsAuthenticated]
 
-    def post(self, request):
-        username = request.data.get('username')
-        password = request.data.get('password')
-        
-        return Response({"username": username, "passoword": password}, status=status.HTTP_200_OK)
+    def get(self, request, *args, **kwargs):
+        users = User.objects.all()
+        serializer = UserSerializer(users, many=True)
+        return Response(serializer.data)
+
+    def post(self, request, format=None):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class CustomTokenObtainPairView(TokenObtainPairView):
+    permission_classes = [AllowAny]  # 認証は必要ありません
