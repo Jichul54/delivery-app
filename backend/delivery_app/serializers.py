1eb65ba6496d6dac175d75133e3ad37fd7b74a57
Yoshimune101 <yoshimune101asano@gmail.com>
2024-03-19 23:27:24 +0900

    token発行処理

--- backend/delivery_app/serializers.py
+++ backend/delivery_app/serializers.py
@@ -1,7 +1,26 @@
 from rest_framework import serializers
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from .models import User
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['id', 'email', 'username', 'post_code', 'address', 'is_active', 'is_staff']
+
+class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+
+        # カスタムのトークンペイロードに追加の情報を含めることができます
+        token['username'] = user.username
+
+        return token
+
+    def validate(self, attrs):
+        data = super().validate(attrs)
+
+        # レスポンスに追加の情報を含めることができます
+        data['username'] = self.user.username
+
+        return data
--- backend/delivery_app/views.py
+++ backend/delivery_app/views.py
@@ -2,12 +2,9 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.authtoken.models import Token
-from django.contrib.auth import authenticate
-from rest_framework.permissions import IsAuthenticated
 from .models.user import User
 from .serializers import UserSerializer
-from rest_framework_simplejwt.views import TokenObtainPairView
-from rest_framework.permissions import AllowAny
+from django.contrib.auth import authenticate
 
 class TestAPIView(APIView):
     def get(self, request, format=None):
@@ -57,5 +54,19 @@ class UsersView(APIView):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-class CustomTokenObtainPairView(TokenObtainPairView):
-    permission_classes = [AllowAny]  # 認証は必要ありません
+class CustomTokenObtainPairView(APIView):
+    def post(self, request):
+        username = request.data.get('username')
+
+        try:
+            # ユーザーを認証する
+            user = User.objects.get(username=username)
+
+            # トークンを生成する
+            token, created = Token.objects.get_or_create(user=user)
+            # トークンを返す
+            return Response({"token": token.key}, status=status.HTTP_200_OK)
+        
+        except User.DoesNotExist:
+            # ユーザーが存在しない場合、エラーメッセージを返す
+            return Response({"error": "User not found"}, status=status.HTTP_404_NOT_FOUND)
