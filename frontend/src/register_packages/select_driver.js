65d1835db738da8d3aa74ccc8bceeb98d025a899
yfujinaka <yuya20021115@gmail.com>
2024-03-20 00:29:07 +0900

    トップバー作成

--- frontend/src/register_packages/select_driver.js
+++ frontend/src/register_packages/select_driver.js
@@ -1,7 +1,27 @@
-import { Box } from '@mui/material';
+import { Box, AppBar, Toolbar, IconButton, Paper, Typography } from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu';
 
 export default function SelectDriver() {
   return (
-    <Box sx={{ flexGrow: 1 }}></Box>
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position='static'>
+        <Paper elevation={2}>
+        <Toolbar>
+          <IconButton
+            size='large'
+            edge='start'
+            color='inehrit'
+            aria-label='menu'
+            sx={{ mr:2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant='body1' component='div' sx={{ flexGrow: 1 }}>
+            モジャモdjango
+          </Typography>
+        </Toolbar>
+        </Paper>
+      </AppBar>
+    </Box>
   )
 }
