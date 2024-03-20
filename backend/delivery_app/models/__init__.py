a55b21935a02fa5b22ce026c9e4350e49f94c290
Akaumigame <106215296+akaumigame6@users.noreply.github.com>
2024-03-21 04:57:54 +0900

    ユーザーテーブルの修正

--- backend/delivery_app/models/__init__.py
+++ backend/delivery_app/models/__init__.py
@@ -2,7 +2,6 @@ from .user import User
 from .company import Company
 from .delivery_status import Delivery_Status
 from .sales_office import Sales_office
-from .driver import Driver
 from .merchandise import Merchandise
 from .order import Order
 from .delivery import Delivery
