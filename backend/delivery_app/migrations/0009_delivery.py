0dea1dabeecfdac06702251fb769427731fd19bd
Akaumigame <106215296+akaumigame6@users.noreply.github.com>
2024-03-25 03:19:58 +0900

    deliveryテーブルの修正

--- backend/delivery_app/migrations/0009_delivery.py
+++ backend/delivery_app/migrations/0009_delivery.py
@@ -16,7 +16,7 @@ class Migration(migrations.Migration):
             name='Delivery',
             fields=[
                 ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('delivery_route_no', models.CharField(max_length=200, verbose_name='delivery_route_no')),
+                ('delivery_route_no', models.IntegerField( verbose_name='delivery_route_no')),
                 ('delivery_status', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='delivery_app.delivery_status')),
                 ('order', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='delivery_app.order')),
                 ('user', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL)),
