c15d4e099c4ddd836a8a78679c6f4ef8ad38d23f
yfujinaka <yuya20021115@gmail.com>
2024-03-26 15:23:13 +0900

    added array-move

--- frontend/src/pages/Drivers/RegisterItems/RegisterItems.js
+++ frontend/src/pages/Drivers/RegisterItems/RegisterItems.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-// import arrayMoveImmutable from 'react-sortable-hoc';
+import { arrayMoveImmutable } from 'array-move';
 import DragIndicatorIcon from '@mui/icons-material/DragIndicator';
 import { IconButton, Box, Stack, List, ListItem, ListItemText, ListItemAvatar, Button, Typography } from '@mui/material';
 import AppBarDriver from '../../../components/AppBar_Driver';
@@ -9,26 +9,42 @@ import { useNavigate } from 'react-router-dom';
 import { getOrders } from '../../../api/get-orders';
 
 
-// const item_list = [];
+const item_list = [
+  {
+    order_id: 1,
+    name: '佐藤',
+    address: '福岡市博多区山王・・・・・・・・'
+  },
+  {
+    order_id: 2,
+    name: '田中',
+    address: '福岡市中央区・・・・・・・・'
+  },
+  {
+    order_id: 4,
+    name: '山崎',
+    address: '福岡市西区・・・・・・・・'
+  }
+]
 
 export default function RegisterItems() {
 
   const navigate = useNavigate();
-  const [ items, setItems ] = React.useState([]);
+  const [ items, setItems ] = React.useState(item_list);
 
   // 注文情報取得
-  const all_orders = getOrders();
-  if (all_orders) {
-    console.log(all_orders);
-  } else {
-    console.log('no order');
-  }
+  // const all_orders = getOrders();
+  // if (all_orders) {
+  //   console.log(all_orders);
+  // } else {
+  //   console.log('no order');
+  // }
 
   // ユーザー情報取得
 
   // 配達物リストの順番変更時
   const onDrop = ({ removedIndex, addedIndex }) => {
-    // const newItems = arrayMoveImmutable(items, removedIndex, addedIndex);
+    const newItems = arrayMoveImmutable(items, removedIndex, addedIndex);
     setItems(newItems);
     console.log(newItems);
   }
@@ -52,10 +68,10 @@ export default function RegisterItems() {
       console.log(order_ids);
 
       // 注文情報から該当するユーザーを検索
-      order_ids.forEach(id => {
-        const this_order = all_orders.find(order => order.id === id);
-        user_ids.push(this_order.user_id);
-      })
+      // order_ids.forEach(id => {
+      //   const this_order = all_orders.find(order => order.id === id);
+      //   user_ids.push(this_order.user_id);
+      // })
 
     }
   }
@@ -64,16 +80,16 @@ export default function RegisterItems() {
   let api_items = [];
   // 登録ボタン押下時
   const handleClick = () => {
-    items.forEach((value, index) => {
-      api_items.push({
-        order_id: value.order_id,
-        delivery_route_no: index
-      })
-      console.log(api_items);
-    })
+  //   items.forEach((value, index) => {
+  //     api_items.push({
+  //       order_id: value.order_id,
+  //       delivery_route_no: index
+  //     })
+  //     console.log(api_items);
+  //   })
 
-    // 確認画面へ遷移
-    navigate(`/confirm-items`);
+  //   // 確認画面へ遷移
+  //   navigate(`/confirm-items`);
   }
 
   return (
